882c167ce4c4b3bcb17d04366f305e0bc3ea9e6b
javas <sivajigsivajig703@gmail.com>
2023-10-20 08:09:17 +0000

    post structure is created

--- SjBlog/client/src/App.jsx
+++ SjBlog/client/src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import { FaUser, FaClock } from 'react-icons/fa';
 import './App.css'
 import i from './assets/p.jpg'
 function App() {
@@ -15,10 +15,61 @@ function App() {
       </nav>
     </header>
     <div className="entries">
+
+
       <div className="entry">
-        <img src={i} class="post" alt='logo' />
-        <h2>This Tech Services Company Has What It Takes to Succeed | The Motley Fool</h2>
+        <div className="image">
+        <img src={i} className="post-image" alt='logo' />       
+        </div>
+       <div className="text">
+        <h2>Exploring the Future: The Rise and Impact of Drones in Modern Technological Era</h2>
+         <p className="info">
+                    <span className="icon-wrapper">
+                        <FaUser className="icon" /> {/* Person Icon */}
+                        <a className="author">Sivajisj</a>
+                    </span>
+                    <span className="icon-wrapper">
+                        <FaClock className="icon" /> {/* Time Icon */}
+                        <time>2023-10-20 13:20</time>
+                    </span>
+                </p>
+        <p className="summary">Drones, once a figment of science fiction, have seamlessly integrated into the fabric
+           of our modern society. Initially developed for military applications, 
+           these flying marvels have diversified into various sectors,
+           from aerial photography .</p>
+        </div>
       </div>
+
+      
+ 
+      
+     
+
+      
+      <div className="entry">
+      <div className="image">
+        <img src={i} className="post-image" alt='logo' />       
+        </div>
+               <div className="text">
+        <h2>Exploring the Future: The Rise and Impact of Drones in Modern Technological Era</h2>
+         <p className="info">
+                    <span className="icon-wrapper">
+                        <FaUser className="icon" /> {/* Person Icon */}
+                        <a className="author">Sivajisj</a>
+                    </span>
+                    <span className="icon-wrapper">
+                        <FaClock className="icon" /> {/* Time Icon */}
+                        <time>2023-10-20 13:20</time>
+                    </span>
+                </p>
+        <p className="summary">Drones, once a figment of science fiction, have seamlessly integrated into the fabric
+           of our modern society. Initially developed for military applications, 
+           these flying marvels have diversified into various sectors,
+           from aerial photography.</p>
+        </div>
+      </div>
+
+      
     </div>
    </main>
     </>
