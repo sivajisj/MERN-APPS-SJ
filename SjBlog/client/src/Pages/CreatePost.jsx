404297b66e33d96e3a065e0c13aad649b4faefe8
javas <sivajigsivajig703@gmail.com>
2023-10-22 12:15:07 +0000

    creater post is configured

--- SjBlog/client/src/Pages/CreatePost.jsx
+++ SjBlog/client/src/Pages/CreatePost.jsx
@@ -6,19 +6,36 @@ const CreatePost = () => {
     const [title , setTitle] = useState('')
     const [summary, setSummary] = useState('')
     const [content , setContent] = useState('')
+    const [files , setFiles] = useState(null)
+
+    async function createNewPost(e){
+     
+      const data = new FormData()
+      data.set('title',title)
+      data.set('summary',summary)
+      data.set('content',content)
+      data.set('file',files[0])
+     
+      e.preventDefault()
+      const response = await fetch('https://5000-sivajisj-mernappssj-yla1k59cxe6.ws-us105.gitpod.io/post',{
+        method: 'POST',
+        body :data
+    
+       })
+    }
    
   return (
    
-    <form>
+    <form enctype="multipart/form-data" onSubmit={createNewPost}>
         <input type="title" placeholder='Title' value={title} onChange={e =>setTitle(e.target.value)} />
-        <input type="titsummaryle" placeholder='Summary' />
-        <input type="file" />
+        <input type="summary" placeholder='Summary' value={summary} onChange={e => setSummary(e.target.value)} />
+        <input type="file" name="file" onChange={e => setFiles(Array.from(e.target.files))}  />
         
         <ReactQuill 
-    value={content} 
+    value={content}  onChange={newVal =>setContent(newVal)}
     modules={modules}  formats={formats}
 />
-        <button style={{marginTop: '15px'}}>Create Post</button>
+        <button type='submit' style={{marginTop: '15px'}}>Create Post</button>
 
     </form>
   )
