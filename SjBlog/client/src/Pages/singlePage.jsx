9d0b8d24d638b9d243d9b636c0183b31638ea3f9
javas <sivajigsivajig703@gmail.com>
2023-10-24 03:40:50 +0000

    completed implementation implementation of single post page

--- SjBlog/client/src/Pages/singlePage.jsx
+++ SjBlog/client/src/Pages/singlePage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+
 
 const SinglePage = () => {
     const [postInfo, setPostInfo] = useState(null);
@@ -20,11 +22,44 @@ const SinglePage = () => {
         .catch(error => {
             console.error("There was an error fetching the post:", error);
         });
-    }, [id]);
-    console.log(postInfo.cover);
+    }, []);
+  
+    // console.log(postInfo.cover);
+    // const title = postInfo.title
+
+  
+    if(!postInfo) return ''
+
+    const formattedDate = new Date(postInfo.createdAt).toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
   return (
-    <div>
-        <img className="singlePageImage" src={`https://5000-sivajisj-mernappssj-yla1k59cxe6.ws-us105.gitpod.io/${postInfo.cover}`}/>
+    <div  className='post-page'>
+   <h1>
+    { postInfo && postInfo.title}
+</h1>
+
+
+<div>
+  {postInfo && <time>{formattedDate}</time>} 
+  <div className="icon-wrapper author">
+                          posted by  <FaUser className="icon" /> {/* Person Icon */}
+                            <a id="sj" className="">@{postInfo.author.username}</a>
+                        </div>
+</div>
+   
+ 
+  <div className='image' >
+  {postInfo && 
+  <img src={`https://5000-sivajisj-mernappssj-yla1k59cxe6.ws-us105.gitpod.io/${postInfo.cover}`} alt="Post Cover"/>}  
+
+  </div>
+ 
+<div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
     </div>
   )
 }
